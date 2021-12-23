18dd76c0a6a6386761fae4ca6c25af1be28e8f5d
niko144 <70695548+niko144@users.noreply.github.com>
2021-12-23 18:28:13 +0530

    abe chal

---
+++ music-list.js
@@ -0,0 +1,116 @@
+let allMusic = [
+  {
+    name: "Driving Over The Top Down",
+    artist: "Iron Man",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Iron Man (2008 version)",
+    artist: "Iron Man",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Merchant of Death",
+    artist: "Iron Man",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Trinkets to Kill a Prince",
+    artist: "Iron Man",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Mark I",
+    artist: "Iron Man",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Fireman",
+    artist: "Iron Man",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Vacation's Over",
+    artist: "Iron Man",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Golden Egg",
+    artist: "Iron Man",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Damn Kid",
+    artist: "Iron Man",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Mark II",
+    artist: "Iron Man",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Extra Dry, Extra Olives",
+    artist: "Iron Man",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Iron Man",
+    artist: "Iron Man",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Gulmira",
+    artist: "Iron Man",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Are Those Bullet Holes?",
+    artist: "Iron Man",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Section 16",
+    artist: "Iron Man",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Iron Monger",
+    artist: "Iron Man",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Arc Reaktor",
+    artist: "Iron Man",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Institutionalized",
+    artist: "Iron Man",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Iron Man (1966 version)",
+    artist: "Iron Man",
+    img: "music-19",
+    src: "music-19"
+  }
+];
